2be44877ff2bae68757dc97e3198f2c6e31d7c8f
Calvin <calvin.lieu@gmail.com>
2022-07-07 14:21:25 -0700

    Added Bookings router

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser,
+} = require("../../utils/auth");
+const { Image, Review, Spot, User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const router = express.Router();
+
+
+
+
+
+
+
+module.exports = router;
